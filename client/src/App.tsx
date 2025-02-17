d9a808e49f8d41384c36b139101fd42f388225cf
romaleev <aromaleev@gmail.com>
2025-02-17 14:01:22 +0700

    add client workspace

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { I18nextProvider } from 'react-i18next'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import EventPage from '#client/pages/EventPage.tsx'
+import darkTheme from '#client/themes/darkTheme.ts'
+import i18n from '#client/i18n.ts'
 
-function App() {
-  const [count, setCount] = useState(0)
+const queryClient = new QueryClient()
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<I18nextProvider i18n={i18n}>
+				<ThemeProvider theme={darkTheme}>
+					<CssBaseline />
+					<BrowserRouter>
+						<Routes>
+							<Route path="/" element={<EventPage />} />
+						</Routes>
+					</BrowserRouter>
+				</ThemeProvider>
+			</I18nextProvider>
+		</QueryClientProvider>
+	)
 }
 
 export default App
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	server: {
 		port: 4200,
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000', // Adjust to match your backend URL
+				changeOrigin: true,
+				secure: false, // Set to false if using self-signed SSL certs
+				rewrite: (path) => path.replace(/^\/api/, '/api'), // Ensures the path remains intact
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			'#root': path.resolve(__dirname),
+			'#client': path.resolve(__dirname, 'src'),
+		},
 	},
 })
